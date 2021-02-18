217a2842bd81d88b38549a37c3a6b0016abadacc
Sarah Rossy <sarahrossysings@gmail.com>
2021-02-18 14:06:26 -0500

    getInterview selector function

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import DayList from "components/DayList";
-// import InterviewerList from "components/InterviewerList";
 import "components/Application.scss";
 import Appointment from "components/Appointment";
+
+import { getAppointmentsForDay, getInterview } from "components/helpers/selectors";
 
 // ------ STEPS TO CREATE A COMPONENT:
 // Create a file with our component name
@@ -12,80 +15,49 @@ import Appointment from "components/Appointment";
 // Write stories for Storybook to render our component in isolation
 // Refactor the hardcoded content to use props & state
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Ringo Starr",
-      interviewer: {
-        id: 1,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  },
-];
-
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
-
 export default function Application(props) {
- const [day, setDay] = useState('Monday');
- 
- const parsedAppointments = appointments.map(appointment =>
-  <Appointment
-    key={appointment.id}
-    { ...appointment }
-    />
-  )
+
+  // combined all states into a single useState object
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  
+  const setDay = (day) => setState({ ...state, day });
+
+  // promise.all takes an array of promises, and returns the responses in an all object
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data
+      }))
+    });
+  }, []) // DO NOT FORGET DEPENDENCY ARRAY!!!
+
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  // returns an array of appointments
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -98,8 +70,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
@@ -110,10 +82,75 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        { parsedAppointments }
+        {schedule}
         <Appointment key="last" time="5pm" />
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
     </main>
   );
 }
+
+// ---- old test data
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
+
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "Ringo Starr",
+//       interviewer: {
+//         id: 1,
+//         name: "Mildred Nazir",
+//         avatar: "https://i.imgur.com/T2WwVfS.png",
+//       }
+//     }
+//   },
+// ];
+
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+// ];
--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -17,3 +17,32 @@ export function getAppointmentsForDay(state, day) {
 //   return  filteredDay ? filteredDay.appointments.map((id) => state.appointments[id]) : [];
 // }
 
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  // make a copy of interview object, put into resultData
+  const resultData = { ...interview };
+
+  //Object.values returns an array of an objects enumerable property values
+  let interviewerInfo = Object.values(state.interviewers)
+
+  // see if the values match, if so, 
+  interviewerInfo.forEach(entry => {
+    if (interview.interviewer === entry.id) {
+      resultData.interviewer = entry;
+    }
+  })
+  return resultData;
+};
+
+//---- should return an object that looks like this:
+// {  
+//   "student": "Lydia Miller-Jones",
+//   "interviewer": {  
+//     "id": 1,
+//     "name": "Sylvia Palmer",
+//     "avatar": "https://i.imgur.com/LpaY82x.png"
+//   }
+// }
